8f2688e2e1fc8bd34cd950360184fdf17f6b08f6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-06-04 20:00:12 +0600

    function work start

---
+++ js/alpha_clash.js
@@ -0,0 +1,10 @@
+// function play(){
+//     // step-1 hide the home screen 
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     // show the playground 
+//     const playgroundSection = document.getElementById('play-ground')
+//     // console.log(playgroundSection.classList);
+//     playgroundSection.classList.remove('hidden');
+// }
